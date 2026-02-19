a794fa4412a2db7a673bfda8faefa0ac23a3005f
Sk. Ajmaeen Ahmed <144755002+Milksheikhh@users.noreply.github.com>
2026-02-19 11:12:27 -0600

    Update outake.cpp

--- src/outake.cpp
+++ src/outake.cpp
@@ -13,17 +13,17 @@ void outake() {
     while (true) {
         double hue = OpticalSensor.hue();
         // Color sorting for red blocks
-        if (Team == "blue" && (hue < 35 || hue > 350)) {
-            wait(0.05, seconds);
-            Outake.spin(reverse, 12.0, voltageUnits::volt);
-            continue;
-        }
+        // if (Team == "blue" && (hue < 35 || hue > 350)) {
+            // wait(0.05, seconds);
+            // Outake.spin(reverse, 12.0, voltageUnits::volt);
+            // continue;
+        // }
         // Color sorting for blue blocks
-        else if (Team == "red" && (hue > 200 && hue < 260)) {
-            wait(0.05, seconds);
-            Outake.spin(reverse, 12.0, voltageUnits::volt);
-            continue;
-        }
+        // else if (Team == "red" && (hue > 200 && hue < 260)) {
+            // wait(0.05, seconds);
+            // Outake.spin(reverse, 12.0, voltageUnits::volt);
+            // continue;
+        // }
 
         // Outake forward: only when driver presses L2, or when in autonomous and explicitly enabled
         if (Auton && AutonOutake) {
